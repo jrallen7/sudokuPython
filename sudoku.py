3c862795b4931fb4ccad9efe7c5f7cc4bdcd3f8c
jrallen7 <james@jamesrallen.xyz>
2020-06-22 00:35:48 -0500

    foo

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,24 @@
+#!/usr/bin/python3
+
+class sudokuCell:
+    def __init__(self):
+        self.val = None
+        self.cands = set(range(1,10))
+
+    def setValue(self, v):
+        assert v in self.cands
+        self.val = v
+        self.cands = set([v])
+
+    def removeCands(self, c):
+        pass
 
 
 
 
+if __name__ == '__main__':
+    foo = sudokuCell()
+    print(foo.cands)
+    foo.setValue(4)
+    print(foo.cands)
 
